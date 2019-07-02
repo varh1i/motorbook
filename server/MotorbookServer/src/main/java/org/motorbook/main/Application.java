057ee0d0c0c589e8f6e84f27e21af73685773176
varhb <Balazs.Varhegyi@pega.com>
2019-07-02 15:41:43 +0200

    Example Rest API

--- server/MotorbookServer/src/main/java/org/motorbook/main/Application.java
+++ server/MotorbookServer/src/main/java/org/motorbook/main/Application.java
@@ -1,8 +1,12 @@
 package org.motorbook.main;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan
+@EnableAutoConfiguration
 @SpringBootApplication
 public class Application {
 
